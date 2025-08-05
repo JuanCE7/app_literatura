dbc4e25c95ba9c258f1779f6a53240996b992197
JuanCE7 <juancastiestrella@gmail.com>
2025-08-04 21:14:56 -0500

    update likes

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -25,7 +25,7 @@ export interface VideoResena {
       autor: "Evelin Alexandra León Macas, Steven Gilbert Sarango con la tutoría de Elvis Adrián Cueva Reatigui",
       duracion: "7:44",
       thumbnail: "https://hablemos-escritoras-as.s3.us-west-1.amazonaws.com/r0o5pvjx81n0w083tdyum67r0sp1",
-      views: "20",
+      views: "29",
       likes: 14,
       videoUrl: "https://drive.google.com/file/d/1Ua8G5xIyF76g-MqHcd3cSZPQ-hdce5nW/view?usp=sharing",
       descripcion: ""
@@ -45,7 +45,7 @@ export interface VideoResena {
       autor: "Yanela Elizabeth Armijos Chamba, Wilfredo Jeovanny Aponte Aponte con la tutoría de Nelly Jacqueline Ochoa Maldonado",
       duracion: "6:55",
       thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf4_BTQEvibOdnniPBFM_pKEoZzh_2Hlo32w&s",
-      views: "18",
+      views: "22",
       likes: 9,
       videoUrl: "https://drive.google.com/file/d/1HH0eXruryrI5tErCOYtK90jJH_CrFPw1/view?usp=sharing",
       descripcion: ""
@@ -55,7 +55,7 @@ export interface VideoResena {
       autor: "Patricia Alvarez, Aracely Chamba con la tutoría de Maribel Jacqueline Ramón Armijos",
       duracion: "8:03",
       thumbnail: "https://latinamericanliteraturetoday.org/wp-content/uploads/2019/11/roza_tumba_quema.jpg",
-      views: "18",
+      views: "17",
       likes: 9,
       videoUrl: "https://www.youtube.com/watch?v=wWyjZUXhZuc&t=8s ",
       descripcion: ""
@@ -66,7 +66,7 @@ export interface VideoResena {
       duracion: "7:09",
       thumbnail: "https://elemblob.blob.core.windows.net/media/las_muertas5385063ba8160_300h.jpg",
       views: "18",
-      likes: 9,
+      likes: 12,
       videoUrl: "https://drive.google.com/file/d/1qGa_YKBM_IIhbTbrzXXdZvs6v8s6oE-_/view ",
       descripcion: ""
     },
@@ -75,8 +75,8 @@ export interface VideoResena {
       autor: "Daniela Tamayo, Bryan Lapo con la tutoría de Ibana Nicole Uchuari Huerta",
       duracion: "7:45",
       thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtrhOIzO9kCDdgqoAqgzGLUabb3hjx2iVVeg&s",
-      views: "10",
-      likes: 9,
+      views: "14",
+      likes: 10,
       videoUrl: "https://drive.google.com/file/d/1djFhamE-Oba6UFZfvxxtOKG5bsPKR3XT/view?usp=drivesdk",
       descripcion: ""
     },
@@ -85,7 +85,7 @@ export interface VideoResena {
       autor: "Geovanny Cabrera, Kenia Cedeño con la tutoría de Yelena Sosoranga Cueva",
       duracion: "7:05",
       thumbnail: "https://lh4.googleusercontent.com/proxy/M-QR_ykHyB7G3hh_4TdonKwIQVAEd85RMGAYjtFIp3M8MzzSkFeO0PU9KUL7etalCy5yB9LUsJXlbbhKjEfjfoME7MpvlkezEy6WLyktsdabSDpePJASAfA7MZVISRC0-Jm0urH8-w",
-      views: "10",
+      views: "19",
       likes: 9,
       videoUrl: "https://youtu.be/TymzFdPQcYM",
       descripcion: ""
@@ -95,8 +95,8 @@ export interface VideoResena {
       autor: "Luisa Parra, Carolina Jumbo con la tutoría de Carlos Salvador Huanca Solano",
       duracion: "7:00",
       thumbnail: "https://images.cdn2.buscalibre.com/fit-in/360x360/e7/8a/e78abf53addd0cb53ea8c222a7349b91.jpg",
-      views: "10",
-      likes: 9,
+      views: "16",
+      likes: 8,
       videoUrl: "https://drive.google.com/file/d/1tJvZGbqq9ItBe-UqYJU7fXzOkkPpg7R2/view?usp=sharing",
       descripcion: ""
     }
