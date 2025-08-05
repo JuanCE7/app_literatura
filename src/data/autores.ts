6bb39935fbebcee18d5ad1bc5c168f04f349fde0
JuanCE7 <juancastiestrella@gmail.com>
2025-08-04 21:21:14 -0500

    5to ciclo

--- src/data/autores.ts
+++ src/data/autores.ts
@@ -135,7 +135,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjXRgiCOFxDdS_flKagFze91t8Wcf8N0xiXYc278u26-CI2vo8K6XhikLFkaWHP_zPSwfVRW2yzM-CzImmsRrrCfxgTuFLhAj5x7frfsLQxD922pUIytRjk4I62kgj0c2EgqtgdKdDazWLAPopwF1kjsJgw8ujOPg-M29cgqS8WR6IzQI1vOihXay60jZybJRtChIHgW7o-PUMEmwsbKQrCNjr5T4H5apYcbbaxnAaNDwbFOMaPhqAzE30yf4JPzhbfgitPcspV71DqwVsU-bdazpc46EphZ9TsYrdVB7aYoqZwoiNHbM5fCKFJ0MEheTpPtkeYb1-BRRa_Ev5TxeW1eB-3wELTu_tPDan6UMC7n6Qq_tHVb7VSmCECAS-xZ93aLCxQkYFBIgCvf1CC3KbCUAlvxEgYI2JOo-8KeBC-hwiXlR_KD2YwQF7eA4zIB56k3CTbLkKADttFBXiOV9YMM7q17lm4cOulQBNxDNVghAr4SXY9tbGVl5SHXhswY9QgU43RnlMaXj8ZZMessOz2vOks2v_t2jhGFNI8Z40C_l7hfztNUCAaYCAf2kOU1AVOobrltvZAEZIUdtfSuDAAjHM4PRL-UlFzzN47QZrBU64o27F49K6aDnMxmjB-lkyN30REDGzRkedlTzcbWJfycuXsnBTyEs4hO4iWE1_9vth1Nf-FaAI1n7NwdeTXLg7cWpvZei9zGHVqUmdvsWHsVbfMSBVkCT0NNnmJ_otmp3WsTQ6_G9JQ0pPU0xG3AYm4RPwnOGJs1zGiVtashmkZXyDETSQhLqL6Tu_S6t75ZPUGt3kCyDFABW0xbU9k3f83Lws2Lfwnfr2X-QRkEYgX45xuOUUJmiCWSEKqFbCQb3J9TGLYH0xNo_6JqS7pkQRrSXAcaYYCIVqn10nYyBph6g1cno4pV5Z6IWeoi3QPuSv3UfXucUkpkcjDMxvubxSZ5TLqqSJfzctuMZzTvyCdIRzRYpY7D=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-orange-400 to-blue-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -148,7 +148,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjVrVXpgDD4KWCayFLVxP_g_Zp3nBLtm801gt5YZhihRDtzX1BR0l-HbrRjcgBkQK9WI4LFjpm0qPXm1C8Yj79T7qToJl07Ys-TlqJrkLLUt7-Sucp782KrgBZ2sZ-yTtrdbDUazIdndYyLhuT-FWNLcWOnHKy7fnSYfqxjP_m0nXtda33uG94e5RvlxE0MVdKI72mz39wCibqPzq1UWop7Cc4lvjDkNqmbFFjPm9oxoWk7b1aMBf8EQbdJQkOHqBcDL3TC8O8X-_wwb3z5LO67BklpTWIFXf2PljOQos-P1On4z_GkM9CRWm6eMUewjfyiq6VH9_L2Qzxfq_0Tv-T5ipu7aVqWP2dK7Z6GYxQRvhwDH6U28Pp4CKHJroshtcLikRI66IdiF5ClHR6oFyvVxWqXPDhGtRo3vtRYfZkPD5taPcVqtW1x8RNZF7MdEvai3zPA20QfxSAiFoZAK1d3wOdU2CbLQxAMOIica8POlpQGzO51H6IsV_XbwNXXqEsajPwggW380cFRAvVdKEEr4-Keka8TVD7gXqqMDXKahCG0ZptCvwCEgSeObNSIvYk3NPLMa4cfBZXq4JFHfjnx4_zMg6a3w1YaMSeeC6qKhFwf79wRn8uZvr55KniSDArowdK-hFo4K11wLrnSGmRBNyOdev56YNQD58Epsoh2gmKlccbX8fXVDUcDC2nLWjICM2XiJw-bvsgX0sSPfOBbmAsh1Iyf5nHfqut3KKAXV7MxFSEl436Nq0zqE-yOGcxfzMcuuhCZp_njKzdhSSP7oXh9w6vj0Gz3WMp8GiJLJ8x7rZfeGjwx1gX7hPPEbXDgqS-sR_rFDd3_WaybKeDv2-8KUFiv7jMDaRspwcZN79nlDhEnUIIsLMRk8iQXWz8I3dfJRFbO_508jotwZmx447zM5R33zw7b-2Fgf-_aMkg0wmXKXe24b4HWv0LyY5CDZ6wxlGj0CQb5ZVzB0okOy3EWFLv0=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-teal-400 to-gray-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -161,7 +161,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjUJkJEGnj82DzvhOx81J2eZWPhyInwVfbntSRYnIx0NQTzeEeYlTqa5jNKMnvJ5BxV4rFGXmCZvQjF8tVMDbPBnquiXJmy3n0LDoIJ0FOHt6SJJgWLa1r2NoFLz1fb2EmsORJ4D6AuKXTsAe5ZhoEcpVX1ZDoaHX8J4kq_-tM6EpzITMgwI_f8PZQTwHXJrdQdFFMyfvU79YW-qpF9nt_1Ti7l5hnlmNI3iKuBHBXI1fEuq-vUuRqEFTQCKVnMBv2y-bB_5dXnLpaAbxsftEF_D7hCMtTDEZBlqDEgZjGMYl50EUPBP-mYe_s4SxKN8bih-AcYFMq-_rfy3Ok-viBZ_18hxyQVoxoX-khSbK1V5TWBdUO7Wbmg02A6Fki6oE6VxjCpVcPfIkAYpCrKUQRzIdFhWhwb7S5EgRgAaVa_s1iXHXDfm5RHsBuTW_-SZQkMa-mgrSvc6wQqRAvmajlbub-L1g0NqiEccEY9tETsFvNHoXvb1hG-Xr2EfBrCcaD3AJjGUDs5BIm06dTDTEFfcOW4rXvl-6eY0uI71E6yVSmRA2wx5Loeygi-qSMPx7wSjlvY1QJo4JwAZWczvb_gUWeJ7o6jVtZ4uq2rVss2nYcbCdQ9cl7Kt6WDKUra_xp6hh5hIILWjIWtNcTsOc40CJZRwtKd1AGmBWOXc_MiEbLbZhVq6MIUYs_-tewm8e7RL7k2VLPnrQdR9u5BCKhx_nBvB9tqQq9EjnCyLh0CrlaArD3yMEPobm_c5XFmV40mg52xlQvtaZI9InzO-ALaJ2AHfpNq2klAzcu076USqX13iUVy2EU1ji1JtkJxldSPfq8MmcMVcD2uCWKeSMIf-k_hEZQ0d7B9hKu3XvYeS74ptNsV5HxvUDjwHy5B8_91NCOmo8igiJItoVTVLuAg9JyfAaAcSWC9AmktlgqrZaChxpQ4SP-0OaowEs_YQwb_RlMiuDb3rvS1WoEREVZKC5on7nw=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-red-400 to-blue-800",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -174,7 +174,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjUkfE0PIx7BX_5KoCWUiN7jG2h9tC3T1_Ss1BDxocySY--YYwvxi7hOR7H1wChr1tlcX_QUHaa9ROn8EPgqhCKQR_versKc4ySLjb-ouJrPrNOJ9omxwLxXFnELJCxQkIgRQrUZ7efmFkTg5fB5X0eSTByAPkLSoH5Joq_ucekBgf-IGl1IBrE32XcRewSFSZ9pN1hReYT2CjF8CzrEj2sSqekC-T6pK-3vKgsosTK193g4oE60CETdQvLJZV58FvvIuzsQANAVVKwbdZoOeRhS0PL8YqTcfMgbF9jFdvnbuu1TxJAIKwT92EhrFCwflFpMnJjBQ7jAVdmaWZLTXyfyVfum4u7oLIOVsvTNaN1ZcyBZzZgKZXtuqUtPEl-ZpFGtKUCBldR7MvEdYn6qQXI9tA1K6sl2edLePbvQY_qQ5CFsU0B5-j0nLOFRTfWF3YRY9iV6TuYfCkbmfhOWdnX9gKou-WUAo_UDP1QQ2XGVJK2grHOv_6ibGA2TPSNXs6rjRpVjnpu7hcNJI6uxK6cnJyV5HFX6JJNtd3EUPrHlCZ_bLdqayRjb5rKwBLcf3dqdl4t-VFJUGuti2XqLew7z6hmwrsdNWLfSlsBICakg8I0c1leA0fawPa22pNvgOrYT8LAD2wb5OqST5nz3iYc7bYkiSYCc7OGhykvaFmsiXW6fBaFnmUFbvfMe3TD3THsReLmypj8a3G8H9_MvLQ44XA_GA5qJo0Ig-l1iIjXouGDlQZqaBVx9CsH1BohxhAgMU-Q-3ZprfVh5Qxm8VUdPb4GOTdRFmbegQWvN_kPYNAcCeJJM-w9cAbWdHjf2yNhDJ6E5M0aBwnBSNbZSwuiu3yO6wL4gTNr1DXy4o8IZA8jrVCxyz4Kx13PImudHJyNpxBdn-M8xcnafGMGgyD2-DeAeKEO-hsESAkw8K2bVonpeqN7ukANowr_VmRS8LSzR-LFXhIczL25F8NX4yd4Ubwkb-g=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-teal-400 to-red-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -187,7 +187,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjWtTgUTiJMsUlsmrodvtWv7LTyPP0PORsor6tKkKYcgQhv8Ua3hwvMdH_gv3EpgpjMGM8-nSa83SydKGWcYrxQxEnndhfb1OzXA_fwEtAu8COC97y5zwn2_gxTJ2j_KXfi1osUzKlK3y90JNFGie89JtG8G2vtN8Od0R-8ZKOMG0HdYuMhAqHQ5PNpIlLfdWS51Mt2B8RJYvaWbcIicgNqGyYxutQrwH5ZqmlGanwD_UG50gJaMpiHjnySItcKo8LcDRzDG4fF6xbcFf68HkBMjmOez1mk-ZGUbgJYubhEoCdbDtsikI5Fyh3YWzZ7yTJEzzfZbeY1VHIjPquhLJN3sACmLduzs0oU0MDzUcq7L-wL7Y9zZEcGGzjpGo_sFGAH5AzmD0NEy-sBF8-O5DWuowtV-6aglpO0sMvcRfw9gqpS-rbPxw8G5qoOsE5AGOXs_TtV40YfanPnMsc7Hx7jdhiiJt23YVYNwoD7Lgi_SBXpGhFOdvTBLGDfiT1o7c3gLk2jkzKHj79oCzo9lqmOsrEdGILxFKT4RT2ZrRRqT6VhNE6M6O8zYwvrQV4c_aWSCySYDlt0dZO7wYTUxLvs95JIKisVEMaRQXAC1-SMqFFuXxhpMRJtAWqUmPtxFQaYsQcqwgodqv_dqGcyw6FD7oH6PoJoTv6xQJkPBATc96-756KuB689fIhS0PwJ_6ztD-891ejYyS2V0MZdUtCkkd2PnSmXHh-h-OOmw3Sr-N5e7Yi44e6b62Cexoh64kMxPzK0j-SgMQ955iYKX0JtlgpTeiVqTZpAU-UDpf47xJJq2xkWmPHFIgG61vKj-kVnof0XOMt3_jt9iDA8JHjTad2DnjKWFrcMKSxfM4GQSAkR_VV-XPt29RQ3UjTCAQ7ggkEpzexasaJNgBDf4qAEy8R8Zyje6M72_Tg_CikmZDHA5BYZ_IJek2h0TLC5opp3G0xUiWjDXL1igQ0c4bPa_W08LIw=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-pink-400 to-blue-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -200,7 +200,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjWslhkE85II4eyK_DtTiffh_diKgNio_to9dy0KVnwYbgxRCo1-bxE_O2nDPhhNbetIFhKn9Ce3AsSYNIaz2gSmuwRxHtTrVTQJUzC6LeMFZ-YPBGFutevn14NoTR_RLznI3b--QVN_T2MBHQewdAeZAxTXBzx043l_RxcmDMvv5wt0R5HXhiPlcPcSPGqkGYlbnp7D1V98yoUupV5_6eFgO8_df2FgLsKnH4D49dEJe14MY37SV36br6qC317XcAadv-z5dVV7iPDjAHlwA4ZGKEA5i-znWPOXwX02HuPP36zzix4FYlp3eM5j7FYVV3V1M2ADFugdWehoc6PHcN47RfvlJ6Q0tt1dvBOFJ8DMsiti5Wk5d4kr-YW_n6LIBwUWUaVXM9lwv81IJQoFJk2xVgsnY1ogu83GZMARxC9_vDQWFGOklkEUobs2DkWfvm-JAIuZp0LIQXh7qSPyztrkJOxHDOBXMzJUQBU2ivKGhy95S0YSXXLHXwgIHcW-fuvl35XVn9V8PbXfzDa2F4fBnvpj8mu0umVY21wgAYpSV3iIMC1wtFk7Fmgz3u4iypmzf2RzmTDdl1s4OGiwKWALzrxy8qW6kaRgWsMCGAM6EBwjvkERheNzsc-roZelUdvxI2Gk4Ts42Wv9oYtuYO4GqEivysUx7CMpsrPO2RoruB_7CpCV5xGPLzhzLVKO3FBAMSB0Jo7pQVT0A2aKQuOF9UeHEIMHKzZC_rGeX_gGcTM1ue7wf_CamKG8qicv1MhoLryQgRl1Y6kjOSn2Jh5a16CMis_r8_JrjnOAQEyI0LGiPobsgJeU7WYuo0xr57jORTu9lL460JE6AmrOOO2zigkuBiNyYo3wqsAYuY3Zn1J9D1WLwIRWqVjttzNSyDmj5iLbJsm9jz56wIJnqhT49skmpW8yqDwvlX8jwiOutV_yjYXvhfJWGDPlSYA-ojCDEzv_3kFShCp-LhBvk0_-BG_UflejJw=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-sky-400 to-sky-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -213,7 +213,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjV8XXyq-I3B30gEdBFvS4yl5UHvyWs_ynJeqhHZux1eMgP9xhpj8CCsjfGvQUMwsKrNwXMxrgs438lCovPWCT4U3u94kJbKKY6zCKEQOBPz6UswoUbTRxqtnIJPgY131UbuQjjTGJagdlSe8yI0rnf7KEauwao8FmdFkHuzSjEWNlj2sEwLxr2M9anfVPasn4SvKYme0VunDFbaW7f1mUqHYlFrs42G-V3TxF4Zi5HX8Z_-3RBIfes1aNxxEflLa6H2RkHBRJ__21k1u2Y9OB18fQy2pBU72aH5HLStmF_oc1d_juQLC_W-LAKY8IS3HhDso-kFGk1ZSObTkLUnTkDQzaH7YflvbWZUVqeA1jGzHiyahNvNC_b1UrOtg3T-rzQFsk6GXA9D-BMNRSAfbBzOrqmsUfo9dCLsunHSZo-1ZRH1xVOFgdX-RDDus3KHcp3CP4vIALTa3m31_a2vtcALGoSxxXXS0uQU9ScUvJ0tldBtd1eo99qvQviVWhHOc5mUiNQjMiRfsFlAtwfH8Cqi2essPCmGj7auS2T1NVgahb0sCs-VEpKsUfd6iC2-dt6zfkkCiYzyH0M7NJ2jsosRRaMLVt9o5sGK-H7zv0gbakYPIsLu4LHARX7R0G0AkMHREYB-V2Zi-YD8AkNK2U8k4ie8uXc4EWzgzeFnGRZ7IRtndR4_pBwN49YKlRzaoySQa_xVOlvFaTbrISSKwro3ABR2rcj1Jn4FRxMvvbn1Yo-WzECcT2DHjPjLw8lwdKTFOH3fWNwz6T2mkkXaD0j4_nBKsVUnq7D5eUXNbaZzU86B_Nr4oF4OU_hrWnJejLKBkdWL1yxI6ZZ-8_ejnGoqHUyFFOcHbgGDOisaPhnuqFbBFBqSlWeQ4lt8XneCmedvehhderwsAJgzV2Blz9hsNShCkWG98Dug6GUIK3umif5VVlDInwFlnWichIsIwKxBO67fq-MX3-WR57bwj-HcWfpiUmM_=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-orange-400 to-sky-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -226,7 +226,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjU977UCqxDElwaEUo8OHhsCtDfoOuHZ9EtOQ9YQz82SjcsOTspr1ewBOPBMu3eToh7AhQDArXFsyXNqMiRBGSzsIveDOiVIZXXPFc5Lw-JBfyUJzYUGDF9zi7I2JYDLpVGYL8aokfNOpG4H-JiKbisES2qTzDXYhBKAHnVe0-KnG9LAm6CplZQa501SNRmT8gQ1POamhL65u0sCnubD73X9r-mRqtQlhgQtMyhbfZ0x4tqH8LEY4s7hS597aQVkFqXr4y3tQemDb5rN6hakS3sZT6DRpGj0BBloFw1tL_h4U6b5q4b2isqU0jZkt-OhdI7gSligokNp6ZkGOegBLChgKGtxHVeNi2G8txcI6XF_e-SV6oDMvu5-XcVuxBO2HP45YALjAfHvYDkZOqh_FhP552ORF1QOfWw4wY8G1S9T6yy7GTmBepvXuXsABI-nLp6N59OCx9NvT_g4tcmueI-p3qXzjTy0iON-co1WoAcY6zc1HNl1yJliWvQqypjJi3R3i08ZIKLjyj78hFKOtFDZudAeQttd3xBDJUHefjcQ82M5iEXTiFy555pdQ66lGABGsG0--CWSCgRcxk4yQFKFCxDblcHc5TtlkwkKaMzVHm9Ef4BlhVmMnxFxu3kjYOfhJuwwaMEYUQyuci261Gpt1GuRKg6X42zDriw3fxldNDfx1j58BLptAZPLpWM0QGajF2lVpioraTPHoJ1gSEPPjq2z7OrCEwnQB5ZN2hr8UsHEofbDeyL-uKYgbEj_lgLd76CL0xf8oUVgF3SDnvxksFqBYK5z5YggalLPBreREsDtjI7jrJARufe5mGvl49rxkeZQUW6SJfT-HYjD_xkNbxwebwPS4c1qe3JklhKUxnBZVURhM-VADjGIe8Gh2vaKKPuLnlqY5RVsE5UFTfuehH0DN51AJK5jYvyOXpIKomnPFdR1uvL9p8Zu2ha-PX2hRmlzdOzf-ZRJbJLX_SMdYNBcA91PdA=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-purple-400 to-blue-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -239,7 +239,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjX9H_1h5seMGztxhXivHhGi7lmzBoXSXV1nWunYhhT4Jo9eX7RHFUWEK946j-EnkIAnMK9FAJTzrK6wJ3NHZ6POVTKQSgQdW_WoaQTHrUxvrttpgv0wUF-GN9ddENsIK88hx8LDCgR7dtptmAzsPOrVKQU0k4XwawANpwlKPDLAQSkaZ1ZI45p3HOfAGUDQ4oHiPsKu_9OkKJTnZCZWCeiBNzdMBaGxH-7etxFIY1Xu6XCHVuDRQCDIktoOL79M_u_dOZ8xCPwtBjudiX4k4-tDKrHea07ORfp5Ncd8eE3VjfSc5E3QqmFoIttPCInwaKZW4YMY56rOK5cwEjbMEVRigqTSIV5oE58IGv1vpTcWFYkTvFshRQq_tV6WpDoyNftivkkY-8EOcUfnV_D_LxEvYeZOwAhPQRME33hJlTF3DrkTO1HmP8d_sTGi3Sw4QKsStpMfzrV-Ckfw8TBltYAozZS02hgNjAL_7o3GtAjk-WGx7Jp1_bYrj2X3zzzodcvIwmRLQi9MqeF50golZt0SR3C7juGPwfchqGCxAxKaupkbURDhPIwJPD6nJMnBaqqENxv_fTR1idHipjQrr0_jm_-6GCxyvR5WZKL26SIu2KV6TunUYkFkejm_KtOwiLYCIljkYupjCWGNPL7aFLlfSofldLPv1NaXTonibYWsgfDdNj8GUzhHkp5O8hCcUAmQ_8JYzWUZ_O1mb5j_8-SO_2YMJWdL8ChTZQL9LytnUsDfeZeUig6Xgec5DN9ll7qM80gm9GRuZ8zbjS2p6r3nWAoto6Ky0qUfJ3YxOs2DLiLaEnHHpsm34HKUbvlJJ37Jn9l5a8ycdTwMNW6jZWvMe9nVNczaVF5N9qtzayWMIEofzwGV_F6fMrSHolOt3GfJ63YcnEM6Kc5UiaOGJf_W6Qx0MY8D2ONsf-kAyjthpefH2JzapwwY_eROfiu-VBMnKy3VuHtlis8dmLmsLssbMthjqRSEsQ=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-purple-900 to-purple-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -252,7 +252,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjVQWiawbqSZSA6y0x7JiETvlRojSd6yZcygeevIRy2-Iv_it29kSdSKKo5U830oCVKQTEyalko5GIRBnAZGNnM8psZwqMhz-otlzSnE0elPDwfEoEb6TyIcJVlikLEagEnF7vxnZr4JmAH_N9ahv4A--EY3igCY-JqhRppbyMZuWa61qRF-NRXOGQpMBqICuTpMxToP-6K7oB7FfnuhF661QyuymFwgd39zQQ8xwggTeBDxEBeNQfELn3ApHj4n4eL1G57ULooEDtWasNy3JmIn-g0KQYjeGtEcpL2wH-9IChZAXeQueLUsH3wKt-4qA4YzC3HM0VCUhdUvgRup8XVHxOxafs0XxLCHkVxYWcWT12rcy5A0_PtfHKmIkg8ktxIQgckgxrurxM98gSU_crbGM-vsEyBF1mZRhurMdYr3CS18TDvnhV8v1PnqNGv2vnq1J9o35kDDzLreRygVAkiqa19IXPgOyzeHPuvgk81DHJy2gyqoDHT_kOE4raYFbGNfljUhLYApFxbc0hvP_BhSlP46Ejr5sV0z3FRdqaHqvdXYmqvwLhhYaEpQqIlfFWMpL60YYE4azzx3HYsLYfmJNB4m3Tdj2FuNCl1uG6xbEXKs9JRWtPL9bvbSJCy_3hyaNBL5ff0y4SUi1MYgo3Efeog0flGyu9bZ5atrwfFqaMSpqyn2HOt9VEUiIb-Qqs8t22IpRJUalQFEwinXvvWS3hZ_i28sRvoH78FWOGwBVMyFeCZ0NmyF1CbLJfwEzpuevG-zEbg5fqlU710EUZC3CW8NHYHR9JWeIR9S4Wv8UUTPgXURuIaFohgRQp6WnwDEKVNmuOQAl4UfXk4P5ucuPEeFxtzJpKPOldcUa0S5aSjDynMzKmqmsigy_rkMnHDZrCkk_F7JyQpV-N0ryxem-CPb9Rz1cClcgAmInf4-EHVPAhHUocos7Mbg3ajBC9AGTLCVqRVCSDHIMLhJI15DBbgYc-viQw=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-yellow-700 to-yellow-400",
       redes: {
         instagram: "@bitacoraliteraria04",
@@ -265,7 +265,7 @@ export interface Autor {
       especialidad: "Literatura Hispanoaméricana",
       ensayos: 1,
       foto: "https://lh3.googleusercontent.com/a-/ALV-UjU7FcoRAscde-X2ab4v5E0dzHeQJ--qkipjyRJ-HIfr98FQRpd-CHJ8qsAcvuu7GuRkLn_yXLJWOAF8Hi_M5XNq8PQmTbuxrNWC5ZizIdpYPnShgvGLIReTg3-tpiIBXab-8klHVlY3aalckUuBLaoQ3VaVBsIdfaqy_Z0Idgiy-hNfgkbUSgydWix4RzNxbaDMNk7PpcqPyo4qIALvqdG6Vl6tgH7YDBt0p2lbkDxHqjPi_kLS-poKqzW88O8h9na5gleGi000B8rukFyeyXy5UrjfKj4h0Hbim4NtJnjnY073HBU6lbOioOfAJPakc67UBGoviLTgK7lW9mVIcUgTtZXr01oVVDS3FW1rhrN0cPBxEI9LvBRs2C5vHV6cVDHTIQwRyf1YuYbChNJ6IQUJLgYy_AxghmrsSw8OwINrnEvRvuPcURZ89-ueW_x-vUPTtr7A7AFmq0OkqUBqaEUP5xZw3TjPYMqhvtCAWgIDb_cfco6vlzCT-m9MVSMHS93V2TcTKqgnitW_v_Wefl_y4KuIsdv5BuzcGxzGJqgo7aKf2xlLfnB4MojM738TOm7Iw-4xzs8WTPgi3UciCIH1UyYqix2_VG6ki7lYPO2Ro6C0_6wzU_qNoYstEULAJ5c0OcqiWoi3-BgfqsXjSz4Ex5RbSYyWsAZU-h6GwGTXUR4RGhNNWk0gBEA8hZQONK8h8Q0myTTtddDSE2vSERE8PspQ-sesIauWgjgBKibBMekQgIgJPUFwXtXRFIfyIkHuigWTpLbJDUSGlmQla2EYEKq63Yylv8R3Gm1e6ec7DIxK9ELiFExOm9DDwst010ldhr13BCkRbdiao2KXsLmQJne46IN38eLoYy--cSNp-iqaWxVjkTOU6rUnj-Dl2TXFkWcXtVEzMHpW6rA0Y7O-Cq1dWV-Qyxexa1oxQfI7Un7rMvhLqJXckBzAaBP2ZKz2nv6st8gdhvMLS-d_FNdSRnaZiXQu=s240-p-k-rw-no",
-      descripcion: "Estudiante de 6to Ciclo de Literatura. ",
+      descripcion: "Estudiante de 5to Ciclo de Literatura. ",
       color: "from-green-400 to-green-900",
       redes: {
         instagram: "@bitacoraliteraria04",
